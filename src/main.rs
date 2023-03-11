e90f91c4601c4e3c29643451344527e9e3dc2767
brisk <bskblake@gmail.com>
2023-03-11 22:25:47 +1030

    Allow command line arguments

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,38 @@ extern crate nalgebra;
 
 use image::{Rgb, RgbImage};
 use nalgebra::{Rotation3, Vector3};
+use std::env;
 
 // Width or height, always square
-const SIZE: u32 = 256;
-const ANGLE: f32 = 15.0 * std::f32::consts::PI / 180.0;
+const DEFAULT_SIZE: u32 = 256;
+const DEFAULT_ANGLE: f32 = 15.0 * std::f32::consts::PI / 180.0;
+
+const USAGE: &str = "
+Create a discretised RGB sphere of tangent-space normals for use in hand-creating normal maps.
+Usage:    
+    normsphere [size [angle]]
+`size` is the pixel width and height of the resulting image (default 256)
+`angle` is the size of the angle step in degrees azimuth and inclination of the normal vector (default 15.0)";
 
 fn main() {
-    let mut img = RgbImage::new(SIZE, SIZE);
-    for y in 0..SIZE {
-        for x in 0..SIZE {
+    let mut args = env::args().skip(1);
+    let size: u32 = match args.next() {
+        Some(x) => x.parse().expect(USAGE),
+        None => DEFAULT_SIZE,
+    };
+
+    let angle: f32 = match args.next() {
+        Some(x) => x.parse::<f32>().expect(USAGE) * std::f32::consts::PI / 180.0,
+        None => DEFAULT_ANGLE,
+    };
+
+    let mut img = RgbImage::new(size, size);
+    for y in 0..size {
+        for x in 0..size {
             // These are the direction they tilt the vector in, not the axes to rotate around
-            let rad_angle = RadAngle::from(Coord { x, y });
-            let angle_around_z = discretise(rad_angle.angle, ANGLE);
-            let tilt_angle = discretise(rad_angle.radius.asin(), ANGLE);
+            let rad_angle = RadAngle::from(Coord { x, y }, size);
+            let angle_around_z = discretise(rad_angle.angle, angle);
+            let tilt_angle = discretise(rad_angle.radius.asin(), angle);
             let norm_vec = Rotation3::from_axis_angle(&Vector3::z_axis(), angle_around_z)
                 * Rotation3::from_axis_angle(&Vector3::y_axis(), tilt_angle)
                 * Vector3::z(); // XXX YOU ARE HERE
@@ -39,13 +58,13 @@ struct Coord {
     y: u32,
 }
 
-impl From<Coord> for RadAngle {
-    fn from(coord: Coord) -> Self {
-        const HALFSIZE: f32 = SIZE as f32 / 2.0;
-        let x = (coord.x as f32) - HALFSIZE;
-        let y = -(coord.y as f32) + HALFSIZE;
+impl RadAngle {
+    fn from(coord: Coord, size: u32) -> Self {
+        let half_size: f32 = size as f32 / 2.0;
+        let x = (coord.x as f32) - half_size;
+        let y = -(coord.y as f32) + half_size;
         Self {
-            radius: x.hypot(y) / HALFSIZE,
+            radius: x.hypot(y) / half_size,
             angle: y.atan2(x),
         }
     }
